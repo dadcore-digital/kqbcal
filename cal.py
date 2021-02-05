e9837061ffc6aba36c92f11a0dcd3ac613001b44
ianfitzpatrick1 <ianfitz@gmail.com>
2021-02-05 17:26:16 -0600

    Added "events" to kqb cal generator

--- cal.py
+++ cal.py
@@ -8,34 +8,54 @@ import pytz
 import requests
 import sys
 
-def get_match_data(params):
+
+def get_api_data(api_path, params=None):
     """
-    Return match data from the Buzz API.
+    Return a list of results from the Buzz API and handle pagination.
+
+    api_path -- Path to query from, e.g. `/matches` or `/events`. Should 
+                include leading but NOT trailing slash. (str)
+    params -- Querystring parameters to pass to API, e.g.
+              `scheduled=true&league=Indy&season=Winter+2021` (str) (optional)
     """
     API_BASE = json.loads(open('settings.json').read())['API_BASE']
-    url = f'{API_BASE}/matches/?{params}'
+    url = f'{API_BASE}{api_path}?{params}'
     
     resp = requests.get(url).json()
-    matches = []
+    entries = []
 
     # Handle paginated results
     if resp['next']:
 
-        while len(matches) < resp['count']:
-            matches += (resp['results'])
+        while len(entries) < resp['count']:
+            entries += (resp['results'])
             url = resp['next']
             if url:
                 resp = requests.get(url).json()
     
     # Just one page!
     else:
-        matches = resp['results']
+        entries = resp['results']
 
-    return matches
+    return entries
 
-def generate_calendar(matches):
+def get_matches(params):
     """
-    Generate a .ics file matches from a dicationary.
+    Return match entries from the Buzz API.
+    """
+    return get_api_data('/matches', params)
+
+
+def get_events(params=None):
+    """
+    Return event entries from the Buzz API.
+    """
+    return get_api_data('/events')
+    
+
+def generate_match_calendar(matches):
+    """
+    Generate a .ics file of matches from a list of dictionaries.
 
     Arguments:
     matches -- A list of dicts containing all matches. (list)
@@ -116,7 +136,59 @@ def generate_calendar(matches):
         cal.events.add(event)
             
     with open('matches.ics', 'w') as cal_file:
-        cal_file.writelines(cal)    
+        cal_file.writelines(cal) 
+
+
+def generate_event_calendar(events):
+    """
+    Generate a .ics file of events from a list of dictionaries.
+
+    Arguments:
+    events -- A list of dicts containing all matches. (list)
+    """
+    cal = Calendar()
+    
+    # Set Calendar Metadata
+    cal.extra.append(ContentLine(name='X-WR-CALNAME', value='KQB Events'))  
+    cal.extra.append(
+        ContentLine(name='X-WR-CALDESC', value='Upcoming matches and events in the Killer Queen Black community.'))  
+    cal.extra.append(ContentLine(name='X-PUBLISHED-TTL', value='PT15M'))  
+
+    # Add all events to calendar
+    for entry in events:
+
+        event = Event()
+
+        event.name = entry['name']
+        
+        event.begin = dp.parse(entry['start_time'])
+        if entry['duration']:
+            date_obj = datetime.strptime(entry['duration'], '%H:%M:%S')
+            delta = timedelta(
+                hours=date_obj.hour, minutes=date_obj.minute,
+                seconds=date_obj.second
+            )
+            event.duration = delta
+        else:
+            event.duration = timedelta(minutes=60)     
+
+        # Tier and Circuit
+        description = entry['description']
+
+        if entry['links']:
+            description += '\n\n[Event Links]\n'
+
+            for link in entry['links']:
+                description += f"\n{link['name']}:\n{link['url']}\n"
+
+        event.description = description
+
+        # Finalize Event
+        cal.events.add(event)
+            
+    with open('events.ics', 'w') as cal_file:
+        cal_file.writelines(cal)
+
 
 if __name__ == '__main__':
     """
@@ -125,6 +197,12 @@ if __name__ == '__main__':
     Arguments:
     params -- API filter querystring parameters to pass to /matches/ endpoint.
     """
-    params = sys.argv[1]
-    matches = get_match_data(params)      
-    generate_calendar(matches)    
+    if 'matches' in sys.argv[1]:
+        params = sys.argv[2]
+        matches = get_matches(params)      
+        generate_match_calendar(matches)
+
+    if 'events'in sys.argv[1]:
+        events = get_events()
+        generate_event_calendar(events)  
+
